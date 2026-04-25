39dbb923232c9694799836a31d66e559cf7dd02a
Nusaybah-web <nusaybahfatin@gmail.com>
2026-04-25 10:46:10 +0200

    Add files via upload

--- practice.py
+++ practice.py
@@ -1,118 +1,61 @@
-"""def m(l,r):
-    result=[]
-    i=j=0
-    while i<len(l) and j<len(r):
-        if l[i]>r[j]:
-            result.append(l[i])
-            i+=1
-        else:
-            result.append(r[j])
-    result.extend(l[i:])
-    result.extend(r[j:])
-    return result
-
 def merge(arr):
+    print("splitting ",arr)
+    l=[]
+    for i in arr:
+        if i%2==0:
+            l.append(i)
     if len(arr)<=1:
         return arr
-    mid=len(arr)//2
-    left=merge(arr[mid:])
-    right=merge(arr[:mid])
-    return m(right,left)
-
-print(merge([23,45,76,27,28,91,75,36]))"""
-
-#decending merge sort
-
-"""def merge(arr):
-    if len(arr)>1:
-        mid=len(arr)//2
-        left=arr[:mid]
-        right=arr[mid:]
-        merge(left)
-        merge(right)
-        i=j=k=0
-        while i<len(left) and j<len(right):
-            if left[i]>right[j]:
-                arr[k]=left[i]
-                i+=1
-            else:
-                arr[k]=right[j]
-                j+=1
-            k+=1
-        while i<len(left):
-            arr[k]=left[i]
-            i+=1
-            k+=1
-        while j<len(right):
-            arr[k]=right[j]
+    mid=len(l)//2
+    left=l[:mid]
+    right=l[mid:]
+    i=j=0
+    k=[]
+    while i<len(left) and j<len(right):
+        if left[i]>right[j]:
+            k.append(right[j])
             j+=1
-            k+=1
-        
+        else:
+            k.append(left[i])
+            i+=1
+    k.extend(left[i:])
+    k.extend(right[j:])
+    print("merging ",k)
+    return k
     
 array=[23,45,76,27,28,91,75,36]
+print(merge(array))
 
-merge(array)
+#palindrum searcher
 
-print(array)
-"""
-
-#word length sorter
-
-"""def sort(arr):
-    if len(arr)>1:
-        mid=len(arr)//2
-        left=arr[:mid]
-        right=arr[mid:]
-        sort(left)
-        sort(right)
-        i=j=k=0
-        while i<len(left) and j<len(right):
-            if len(left[i])<len(right[j]):
-                arr[k]=left[i]
-                i+=1
-            else:
-                arr[k]=right[j]
-                j+=1
-            k+=1
-        while i<len(left):
-            arr[k]=left[i]
-            i+=1
-            k+=1
-        while j<len(right):
-            arr[k]=right[j]
-            j+=1
-            k+=1
-
-array=["elephant", "cat", "dog", "hippopotamus", "ant"]
-
-sort(array)
-
-print(array)"""
-
-#merge two sorted lists
-
-def sort(a,b):
-    i=j=0
-    l=[]
-    while i<len(a) and j<len(b):
-        if a[i]<b[j]:
-            l.append(a[i])
-            i+=1
-        else:
-            l.append(b[j])
-            j+=1
-    while i<len(a):
-        l.append(a[i])
-        i+=1
-    while j<len(b):
-        l.append(b[j])
-        j+=1
-
-    return l
+"""def wordreverse(inp):
+    stack=[]
+    for i in inp:
+        stack.append(i)
+    for i in inp:
+        if i!=stack.pop():
+            return False
+    return True
         
-l1=[1,3,5,7,9]
-l2=[0,2,4,6,8]
 
-results=sort(l1,l2)
+w=wordreverse("racecar")
 
-print(results)
+print(w)"""
+
+#binary converter
+
+"""def binary(num):
+    stack=[]
+    while num>0:
+        r=num%2
+        stack.append(r)
+        num=num//2
+    binary=""
+    while stack:
+        binary+=str(stack.pop())
+    return binary
+
+b=binary(10)
+
+print(b)"""
+
